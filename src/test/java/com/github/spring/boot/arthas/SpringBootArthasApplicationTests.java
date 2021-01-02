2dd30e63e7f5818fa5364158e5b1cc22beba0fa7
shishaodong <spring-boot@qq.com>
2021-01-02 22:45:44 +0800

    Initial commit

---
+++ src/test/java/com/github/spring/boot/arthas/SpringBootArthasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.spring.boot.arthas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootArthasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
