bf38bd270e79176fdead0badf270e8ce2f4eeca0
shishaodong <spring-boot@qq.com>
2021-01-04 19:36:30 +0800

    使用 mvn spring-boot:build-image

---
+++ src/main/java/com/github/spring/boot/arthas/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.github.spring.boot.arthas.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 石少东
+ * @date create in 2021/1/3 6:47 下午
+ */
+@RestController
+public class HelloController {
+
+    @GetMapping("")
+    public String hello(){
+        return "123456\n";
+    }
+
+
+}
